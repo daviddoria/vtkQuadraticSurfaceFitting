45af276d06c1b9b9fc5295bd1862c3c737c548dc
David Doria <daviddoria@gmail.com>
2010-10-10 18:18:58 -0400

    ENH: allow fitting of surfaces which are not functions of x,y

--- vtkQuadricSurfaceFitting.cxx
+++ vtkQuadricSurfaceFitting.cxx
@@ -1,11 +1,16 @@
 #include "vtkQuadricSurfaceFitting.h"
+#include "vtkBestFitPlane.h"
 
 #include "vtkObjectFactory.h"
+#include "vtkLandmarkTransform.h"
+#include "vtkTransformPolyDataFilter.h"
+#include "vtkFieldData.h"
 #include "vtkStreamingDemandDrivenPipeline.h"
 #include "vtkInformationVector.h"
 #include "vtkInformation.h"
-
 #include "vtkPolyData.h"
+#include "vtkDoubleArray.h"
+#include "vtkPlane.h"
 #include "vtkImageData.h"
 #include "vtkSmartPointer.h"
 #include "vtkMath.h"
@@ -59,6 +64,54 @@ int vtkQuadricSurfaceFitting::RequestData(
   vtkPolyData *output = vtkPolyData::SafeDownCast(
 		  outInfo->Get(vtkDataObject::DATA_OBJECT()));
 
+  // Compute the best fit plane.
+  vtkSmartPointer<vtkBestFitPlane> bestFitFilter =
+    vtkSmartPointer<vtkBestFitPlane>::New();
+  bestFitFilter->SetInputConnection(input->GetProducerPort());
+  bestFitFilter->Update();
+
+  double bestFitNormal[3];
+  vtkDoubleArray* normalArray = vtkDoubleArray::SafeDownCast(bestFitFilter->
+                      GetOutput()->GetFieldData()->GetArray("BestFitPlaneNormal"));
+  normalArray->GetTupleValue(0,bestFitNormal);
+
+  double bestFitOrigin[3];
+  vtkDoubleArray* originArray = vtkDoubleArray::SafeDownCast(bestFitFilter->
+                      GetOutput()->GetFieldData()->GetArray("BestFitPlaneOrigin"));
+  originArray->GetTupleValue(0,bestFitOrigin);
+
+  // Find a coordinate system on the plane
+  double v1[3] = {bestFitNormal[0], bestFitNormal[1], bestFitNormal[2]};
+  double v2[3];
+  double v3[3];
+  vtkMath::Perpendiculars(v1, v2, v3, 0);
+
+  // Project points onto the plane
+  vtkSmartPointer<vtkPoints> transformedPoints =
+    vtkSmartPointer<vtkPoints>::New();
+
+  for(vtkIdType i = 0; i < input->GetNumberOfPoints(); i++)
+    {
+    double p[3];
+    input->GetPoint(i,p);
+    double projected[3];
+    vtkPlane::ProjectPoint(p, bestFitOrigin, bestFitNormal, projected);
+    // Find the vector from the plane origin to the projected point in the new coordinate system
+    // by projecting the vector from the plane origin to the projected point in the normal coordinate
+    // system onto v2 and v2. This vector projected onto v1 is the height needed for the
+    // least squares problem.
+    double oldV[3];
+    vtkMath::Subtract(projected, bestFitOrigin, oldV);
+    double new1[3];
+    double new2[3];
+    double new3[3];
+    vtkMath::ProjectVector(oldV, v1, new1);
+    vtkMath::ProjectVector(oldV, v2, new2);
+    vtkMath::ProjectVector(oldV, v3, new3);
+
+    transformedPoints->InsertNextPoint(vtkMath::Norm(new2), vtkMath::Norm(new3), vtkMath::Norm(new1));
+    }
+
   // Fit a quadric surface
   int numberOfSamples = input->GetNumberOfPoints();
   int numberOfVariables = 6;
@@ -85,14 +138,6 @@ int vtkQuadricSurfaceFitting::RequestData(
 
   vtkMath::SolveLeastSquares(numberOfSamples, x, numberOfVariables, y, 1, m);
 
-  std::cout << "Solution is: " << std::endl;
-  for(unsigned int i = 0; i < numberOfVariables; i++)
-    {
-    std::cout << m[i][0] << " ";
-    }
-
-  std::cout << std::endl;
-
   // Create the quadric function definition
   vtkSmartPointer<vtkQuadric> quadric =
     vtkSmartPointer<vtkQuadric>::New();
@@ -100,7 +145,8 @@ int vtkQuadricSurfaceFitting::RequestData(
   quadric->SetCoefficients(m[0][0], m[1][0], 0, m[2][0], 0, 0, m[3][0], m[4][0], -1, m[5][0]);
 
   // Sample the quadric function
-  vtkSmartPointer<vtkSampleFunction> sample = vtkSmartPointer<vtkSampleFunction>::New();
+  vtkSmartPointer<vtkSampleFunction> sample =
+    vtkSmartPointer<vtkSampleFunction>::New();
   sample->SetSampleDimensions(50,50,50);
   sample->SetImplicitFunction(quadric);
 
@@ -115,7 +161,8 @@ int vtkQuadricSurfaceFitting::RequestData(
   inputBounds[5] += border;
   sample->SetModelBounds(inputBounds);
 
-  vtkSmartPointer<vtkContourFilter> contours = vtkSmartPointer<vtkContourFilter>::New();
+  vtkSmartPointer<vtkContourFilter> contours =
+    vtkSmartPointer<vtkContourFilter>::New();
   contours->SetInput(sample->GetOutput());
   contours->GenerateValues(1, 0.0, 0.0);
   contours->Update();
@@ -123,7 +170,50 @@ int vtkQuadricSurfaceFitting::RequestData(
   free_matrix(x);
   free_matrix(m);
 
-  output->ShallowCopy(contours->GetOutput());
+
+  double sourceOrigin[3];
+  for(unsigned int i = 0; i < 3; i++)
+    {
+    sourceOrigin[i] = bestFitOrigin[i];
+    }
+
+  double sourceP1[3];
+  vtkMath::Add(sourceOrigin, v1, sourceP1);
+
+  double sourceP2[3];
+  vtkMath::Add(sourceOrigin, v2, sourceP2);
+
+  double sourceP3[3];
+  vtkMath::Add(sourceOrigin, v3, sourceP3);
+
+  vtkSmartPointer<vtkPoints> sourcePoints =
+    vtkSmartPointer<vtkPoints>::New();
+  sourcePoints->InsertNextPoint(sourceOrigin);
+  sourcePoints->InsertNextPoint(sourceP1);
+  sourcePoints->InsertNextPoint(sourceP2);
+  sourcePoints->InsertNextPoint(sourceP3);
+
+  vtkSmartPointer<vtkPoints> targetPoints =
+    vtkSmartPointer<vtkPoints>::New();
+  targetPoints->InsertNextPoint(0,0,0);
+  targetPoints->InsertNextPoint(1,0,0);
+  targetPoints->InsertNextPoint(0,1,0);
+  targetPoints->InsertNextPoint(0,0,1);
+
+  // Find the coordinates of the projected points in the new coordinate system
+  vtkSmartPointer<vtkLandmarkTransform> landmarkTransform =
+    vtkSmartPointer<vtkLandmarkTransform>::New();
+  landmarkTransform->SetSourceLandmarks(sourcePoints);
+  landmarkTransform->SetTargetLandmarks(targetPoints);
+  landmarkTransform->SetModeToRigidBody();
+  landmarkTransform->Update();
+
+  vtkSmartPointer<vtkTransformPolyDataFilter> transformFilter =
+    vtkSmartPointer<vtkTransformPolyDataFilter>::New();
+  transformFilter->SetInputConnection(contours->GetOutputPort());
+  transformFilter->SetTransform(landmarkTransform);
+
+  output->ShallowCopy(transformFilter->GetOutput());
 
   return 1;
 }
